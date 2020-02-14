47c7e297ee8f19b1efb8ab284af457d92cb4dac9
Jeff Hall <JeffH@vulcan.com>
2020-02-14 13:29:39 -0800

    Read SauceLabs credentials from env variables

--- wdio.conf.js
+++ wdio.conf.js
@@ -2,15 +2,11 @@ exports.config = {
 	specs: [
 		'./test/**/*.js'
 	],
-	// Patterns to exclude.
-	exclude: [
-		// 'path/to/excluded/files'
-	],
 	maxInstances: 5,
 	logLevel: 'info',
 	services: ['sauce'],
-	user: 'jeffhall1234',
-	key: '46a2b968-7cbc-4e55-840c-4f7f0f3d16c2',
+	user: process.env.SAUCE_USERNAME,
+	key: process.env.SAUCE_ACCESS_KEY,
 	sauceConnect: false,
 	region: 'us',
 	capabilities: [{
